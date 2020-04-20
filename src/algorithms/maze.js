cae9f11c92558fc3a2153fc083936e22f677a380
Kevin <Kevin_Saephanh@yahoo.com>
2020-04-19 23:53:29 -0500

    Fixed wall breaking, but the same maze is generated over and over

--- src/algorithms/maze.js
+++ src/algorithms/maze.js
@@ -1,40 +1,32 @@
-const MAX_ROWS = 20;
-const MAX_COLS = 20;
-
-export const initGrid = () => {
-    const grid = [];
-    for (let row = 0; row < 20; row++) {
-        grid[row] = []; // Current row
-        for (let col = 0; col < 20; col++) {
-            const cell = new Cell(row, col);
-            grid[row].push(cell);
-        }
-    }
-    return grid;
-};
-
 export const primsMaze = (grid) => {
     const row = Math.floor(Math.random() * 20);
     const col = Math.floor(Math.random() * 20);
     const rootCell = grid[row][col];
-    rootCell.visited = true;
     const toVisit = [];
+    const maze = [];
 
     toVisit.push(rootCell);
     while (toVisit.length > 0) {
         const currCell = toVisit.pop();
+        currCell.visited = true;
         console.log(currCell);
+        if (!maze.includes(currCell)) maze.push(currCell);
 
-        const frontiers = currCell.getFrontiers(grid);
+        const frontiers = getFrontiers(grid, row, col);
         if (frontiers.length > 0) {
             // Randomize frontiers then traverse them
             const shuffledFrontiers = shuffle(frontiers);
             shuffledFrontiers.forEach((frontier) => {
-                frontier.visited = true;
-                breakWall(currCell, frontier);
-                toVisit.push(frontier);
+                frontier = grid[frontier[0]][frontier[1]];
+                if (!frontier.visited) {
+                    breakWall(currCell, frontier);
+                    toVisit.push(frontier);
+                }
             });
         }
+        toVisit.forEach((n) => {
+            console.log(n);
+        });
     }
 
     return grid;
@@ -45,26 +37,28 @@ const breakWall = (currCell, frontier) => {
     const col1 = currCell.col;
     const row2 = frontier.row;
     const col2 = frontier.col;
+    const x = col1 - col2;
+    const y = row1 - row2;
 
     // Break top wall
-    if (row1 - 1 === row2 && col1 === col2) {
+    if (y === 1) {
         currCell.walls[0] = false;
-        frontier.walls[2] = false;
+        frontier.walls[1] = false;
     }
     // Break bottom wall
-    else if (row1 + 1 === row2 && col1 === col2) {
-        currCell.walls[2] = false;
+    else if (y === -1) {
+        currCell.walls[1] = false;
         frontier.walls[0] = false;
     }
     // Break right wall
-    else if (row1 === row2 && col1 + 1 === col2) {
-        currCell.walls[3] = false;
-        frontier.walls[1] = false;
+    if (x === 1) {
+        currCell.walls[2] = false;
+        frontier.walls[3] = false;
     }
     // Break left wall
-    else {
-        currCell.walls[1] = false;
-        frontier.walls[3] = false;
+    else if (x === -1) {
+        currCell.walls[3] = false;
+        frontier.walls[2] = false;
     }
 };
 
@@ -78,60 +72,39 @@ const shuffle = (arr) => {
     return arr;
 };
 
-class Cell {
-    constructor(row, col) {
-        this.row = row;
-        this.col = col;
-        this.visited = false;
-        this.walls = [true, true, true, true];
+const getFrontiers = (grid, row, col) => {
+    let top = [row - 1, col];
+    let bottom = [row + 1, col];
+    let right = [row, col + 1];
+    let left = [row, col - 1];
+    const frontiers = [];
+
+    if (isValidFrontier(top, grid)) {
+        frontiers.push(top);
     }
 
-    getFrontiers(grid) {
-        let top = [this.row - 1, this.col];
-        let bottom = [this.row + 1, this.col];
-        let right = [this.row, this.col + 1];
-        let left = [this.row, this.col - 1];
-        const frontiers = [];
-
-        if (this.isEligibleFrontier(top, grid)) {
-            top = grid[top[0]][top[1]];
-            frontiers.push(top);
-        }
-
-        if (this.isEligibleFrontier(bottom, grid)) {
-            bottom = grid[bottom[0]][bottom[1]];
-            frontiers.push(bottom);
-        }
-
-        if (this.isEligibleFrontier(right, grid)) {
-            right = grid[right[0]][right[1]];
-            frontiers.push(right);
-        }
-
-        if (this.isEligibleFrontier(left, grid)) {
-            left = grid[left[0]][left[1]];
-            frontiers.push(left);
-        }
-
-        return frontiers;
+    if (isValidFrontier(bottom, grid)) {
+        frontiers.push(bottom);
     }
 
-    isEligibleFrontier(frontier, grid) {
-        if (this.isValidFrontier(frontier)) {
-            const x = frontier[0];
-            const y = frontier[1];
-            frontier = grid[x][y];
-            return !frontier.visited;
-        }
+    if (isValidFrontier(right, grid)) {
+        frontiers.push(right);
     }
 
-    // This method checks a frontier to see if it lies within the bounds of the grid
-    isValidFrontier(frontier) {
-        return (
-            frontier[0] < MAX_ROWS &&
-            frontier[0] >= 0 &&
-            frontier[1] < MAX_COLS &&
-            frontier[1] >= 0
-        );
+    if (isValidFrontier(left, grid)) {
+        frontiers.push(left);
     }
-}
+    console.log(frontiers);
+
+    return frontiers;
+};
+
+// This method checks a frontier to see if it lies within the bounds of the grid
+const isValidFrontier = (frontier, grid) => {
+    return (
+        frontier[0] < grid.length &&
+        frontier[0] >= 0 &&
+        frontier[1] < grid.length &&
+        frontier[1] >= 0
+    );
+};
--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Grid.css";
 import { Cell } from "../Cell/Cell";
-import { initGrid, primsMaze } from "../../algorithms/maze";
+import { primsMaze } from "../../algorithms/maze";
 
 export const Grid = (props) => {
     const [grid, setGrid] = useState([]);
@@ -10,15 +10,37 @@ export const Grid = (props) => {
         console.log(props.status);
         switch (props.status) {
             case "generate":
-                setGrid(primsMaze(grid));
+                const primsGrid = primsMaze(grid);
+                setGrid(primsGrid);
                 break;
             case "start":
                 break;
             default:
-                setGrid(initGrid());
+                setGrid(initGrid);
                 break;
         }
     }, [props.status]);
+
+    const initGrid = () => {
+        const newGrid = [];
+        for (let row = 0; row < 20; row++) {
+            newGrid[row] = []; // Current row
+            for (let col = 0; col < 20; col++) {
+                const cell = createCell(row, col);
+                newGrid[row].push(cell);
+            }
+        }
+        return newGrid;
+    };
+
+    const createCell = (row, col) => {
+        return {
+            row,
+            col,
+            visited: false,
+            walls: [true, true, true, true],
+        };
+    };
 
     return (
         <table className="grid">
